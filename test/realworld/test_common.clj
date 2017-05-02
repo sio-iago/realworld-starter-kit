d6a507f487fe76cbc2e106e47cc4d227f7472346
Iago Oliveira <silva.io@outlook.com>
2017-05-02 00:44:48 -0300

    Removing tests, since its being made with postman

--- test/realworld/test_common.clj
+++
@@ -1,7 +0,0 @@
-(ns realworld.test-common
-  (:use [realworld.routes :only [app]]
-        [clojure.data.json :only [read-str]]))
-
-(def test-app (app))
-
-(defn read-json [str] (read-str str :key-fn keyword))
